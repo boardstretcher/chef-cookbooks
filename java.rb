89a25b5e78b7e3a4cd04066f65e81d5741632d8f
boardstretcher <szornes@outlook.com>
2017-06-14 13:40:16 -0400

    Create java.rb

---
+++ java.rb
@@ -0,0 +1,7 @@
+# pulling down oracle jdk
+
+remote_file 'jdk-8u131-linux-x64.tar.gz' do
+ source 'http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz'
+ headers ({ "Cookie" => "oraclelicense=a" })
+ action :create
+end
